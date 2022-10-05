3ccd6d9abc6a9f8fdfeb66a64e377e0f360c2336
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2022-10-05 14:50:31 -0400

    able to parse the json file filtering funny quotes

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import json
 
 
 def main():
-    print("Hello World!")
+   # parsing json file
+    with open("data/quotes.json") as json_file:
+        data = json.load(json_file)
+
+    funny_quotes = find_funny_quotes(data)
+
+    print(funny_quotes)
+
+
+def find_funny_quotes(data):
+    funny_quotes = list()
+
+    for element in data:
+        if "funny" in element["Tags"]:
+            funny_quotes.append(element)
+
+    return funny_quotes
 
 
 if __name__ == "__main__":
